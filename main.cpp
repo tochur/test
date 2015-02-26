f215645e8ff178eb260e83471db7fcfabf2c2e24
tochur <tochur@gmail.com>
2015-02-26 21:00:00 +0100

    innfo add

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
+#include<string>
 using namespace std;
+
+class Info{
+    int age;
+    string name;
+    Info(int age = 20, string name = "Antek"){
+        cout<<"Default constructor"<<endl;
+    }
+    void print(){
+        cout<<"Imie: "<<name<<", wiek: "<<age<<endl;
+    }
+};
 
 int main(){
     cout<<"Antex git test with C++ main"<<endl;
+    Info info = Info();
+
     return 0;
 }
